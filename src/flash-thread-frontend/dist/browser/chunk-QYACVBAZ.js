d28d5d9be689158c6e4820fd2da4b44fd036a892
Your Name <you@example.com>
2024-01-26 18:30:23 +0400

    build

---
+++ src/flash-thread-frontend/dist/browser/chunk-QYACVBAZ.js
@@ -0,0 +1,1 @@
+import{b as a}from"./chunk-KAPUGOXL.js";import"./chunk-3YEIR67D.js";import"./chunk-PBIZOYDX.js";import"./chunk-BI7MKFOP.js";import"./chunk-JMZ7DUBM.js";export{a as ChatComponent};
---
+++ src/flash-thread-frontend/dist/browser/chunk-SNVR6GVX.js
@@ -0,0 +1,1 @@
+import{j as a}from"./chunk-HLT4T6DQ.js";import"./chunk-GER4ALVS.js";import"./chunk-KAPUGOXL.js";import"./chunk-3YEIR67D.js";import"./chunk-LCP4AOTT.js";import"./chunk-MAH65QIO.js";import"./chunk-PBIZOYDX.js";import"./chunk-BI7MKFOP.js";import"./chunk-JMZ7DUBM.js";export{a as MainComponent};
